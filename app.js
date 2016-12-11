20aade2d7b0a13f4875373dd73777bf8fbd9061f
marcus gray <abundanceisfree@gmail.com>
2016-12-10 20:47:41 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+require('aframe');
+
+
+var img = new Image();
+img.crossOrigin = "http://i.imgur.com/16JlJg4.jpg";  // or "anonymous", will be interpreted the same
+...
+img.src = "http://i.imgur.com/16JlJg4.jpg";
